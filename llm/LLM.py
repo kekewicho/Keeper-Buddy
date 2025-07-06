9909bd4d271be4012e0298a2e53624944021901a
kekewicho <luisruben2997@gmail.com>
2025-07-05 21:20:38 -0600

    feat: LLM quota exceed error handling

--- llm/LLM.py
+++ llm/LLM.py
@@ -11,21 +11,35 @@ genai.configure(api_key=os.getenv("GOOGLE_AI_API_KEY"))
 tasks_tools_definition = json.loads(open("tools.json", "r").read())
 tools = [types.Tool(function_declarations=tasks_tools_definition)]
 
-MODEL_NAME = "gemini-1.5-flash"
+MODEL_NAME = "gemini-2.5-flash"
 
 
 def prompt(contents: list) -> list:
-    
-    model = genai.GenerativeModel(
-        MODEL_NAME
-    )
-    
-    response = model.generate_content(
-        contents=contents,
-        tools=tools,
-    )
+    try:
+        model = genai.GenerativeModel(
+            MODEL_NAME
+        )
+        
+        response = model.generate_content(
+            contents=contents,
+            tools=tools,
+        )
 
-    return [
-        *contents,
-        response.to_dict()['candidates'][0]['content']
-    ]
+        return [
+            *contents,
+            response.to_dict()['candidates'][0]['content']
+        ]
+    
+    except Exception as e:
+
+        return [
+            *contents,
+            {
+                "role": "model",
+                "parts": [
+                    {
+                        "text": f"Ocurrio un error al procesar la solicitud: {str(e)}"
+                    }
+                ]
+            }
+        ]
